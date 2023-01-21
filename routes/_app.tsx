a2836f186dbf6657ec3d9a81cac091274b7901db
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-01-21 15:10:18 +0900

    feat: twitter:creatorを削除

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,9 @@
-import { Head, asset } from "$fresh/runtime.ts";
+import { asset, Head } from "$fresh/runtime.ts";
 import { AppProps } from "$fresh/server.ts";
 
 export default function App(props: AppProps) {
   const title = "im@sparql Data Dashboard";
   const desc = "im@sparqlのデータダッシュボード的なものです";
-  const creator = "@arrow_2nd";
 
   return (
     <>
@@ -13,19 +12,18 @@ export default function App(props: AppProps) {
         <meta name="description" content={desc} />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={desc} />
-        <meta name="twitter:creator" content={creator} />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={desc} />
         <link
           rel="stylesheet"
           href={asset(
-            "https://api.fonts.coollabs.io/css2?family=M+PLUS+Rounded+1c:wght@400;500&display=swap"
+            "https://api.fonts.coollabs.io/css2?family=M+PLUS+Rounded+1c:wght@400;500&display=swap",
           )}
         />
         <link
           rel="stylesheet"
           href={asset(
-            "https://cdn.jsdelivr.net/npm/@tabler/icons@1.76.0/iconfont/tabler-icons.min.css"
+            "https://cdn.jsdelivr.net/npm/@tabler/icons@1.76.0/iconfont/tabler-icons.min.css",
           )}
         />
         <link rel="stylesheet" href={asset("./globals.css")} />
